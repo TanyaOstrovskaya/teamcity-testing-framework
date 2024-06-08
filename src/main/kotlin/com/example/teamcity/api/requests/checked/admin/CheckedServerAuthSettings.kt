47258451257821f0e71c0b0994126dee57e73f7f
TanyaOstrovskaya <ostrtanja@gmail.com>
2024-06-08 08:01:52 +0300

    add Swagger coverage

--- src/main/kotlin/com/example/teamcity/api/requests/checked/admin/CheckedServerAuthSettings.kt
+++ src/main/kotlin/com/example/teamcity/api/requests/checked/admin/CheckedServerAuthSettings.kt
@@ -2,6 +2,7 @@ package com.example.teamcity.api.requests.checked.admin
 
 import com.example.teamcity.api.models.ServerAuthSettings
 import com.example.teamcity.api.requests.Request
+import com.github.viclovsky.swagger.coverage.SwaggerCoverageRestAssured
 import io.restassured.RestAssured
 import io.restassured.specification.RequestSpecification
 import org.apache.http.HttpStatus.SC_OK
@@ -11,13 +12,16 @@ private const val AUTH_SETTINGS_ENDPOINT = "/app/rest/server/authSettings"
 class CheckedServerAuthSettings(spec: RequestSpecification): Request(spec) {
 
     fun get(): ServerAuthSettings {
-        return RestAssured.given().spec(spec).get(AUTH_SETTINGS_ENDPOINT)
+        return RestAssured.given()
+            .filter(SwaggerCoverageRestAssured())
+            .spec(spec).get(AUTH_SETTINGS_ENDPOINT)
             .then().assertThat().statusCode(SC_OK)
             .extract().`as`(ServerAuthSettings::class.java)
     }
 
     fun update(settings: ServerAuthSettings): String {
         return RestAssured.given()
+            .filter(SwaggerCoverageRestAssured())
             .spec(spec)
             .body(settings)
             .put(AUTH_SETTINGS_ENDPOINT)
